b89ca88ce6c162818124c7950989c41faccc3c31
Vitória Lorramy <134651005+vitorialorramymorais@users.noreply.github.com>
2024-09-20 11:28:59 -0400

    Add files via upload

---
+++ server-calme.js
@@ -0,0 +1,44 @@
+/*
+ * Servidor que realiza operação de soma de dois parâmetros na url
+ */
+
+// importar módulo/bibliotecas
+const http = require("http");
+const url = require("url");
+
+// configurações de acesso ao servidor
+const localhost = "127.0.0.1";
+const port = 3000;
+
+// configuração do servidor
+const server = http.createServer((req, res) => {
+  // 2 instruções de configuração do servidor (navegador)
+  res.statusCode = 200; // status ok
+  res.setHeader("Content-Type", "text/html; charset=utf-8"); // tipo do conteúdo (formato HTML)
+
+  let q = url.parse(req.url, true).query; // consultando a string da url
+
+  // exemplo de url
+  // http://127.0.0.1:3000/?n1=2&n2=3
+
+  // montagem da mensagem de retorno (template string)
+  let n1 = Number(q.n1);
+  let n2 = Number(q.n2);
+  let txt = `
+    A soma de ${n1} + ${n2} = <b>${n1 + n2}</b><br>
+    A subtração de ${n1} - ${n2} = <b>${n1 - n2}</b><br>
+    A divisão de ${n1} / ${n2} = <b>${n1 / n2}</b><br>
+    A multiplicação de ${n1} * ${n2} = <b>${n1 * n2}</b>
+  `;
+
+  res.write(txt); // mensagem no <body> da página
+  // res.write('Hello World!!');
+  res.end(); // final da resposta
+});
+
+// "rodar" o servidor
+server.listen(
+  port, // porta de acesso ao servidor
+  localhost, // endereço (IP) do servidor
+  console.log("Server runinng...")
+);
